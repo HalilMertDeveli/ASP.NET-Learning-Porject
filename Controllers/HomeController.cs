241fa697dffeae2c4a9422b801e342e120dc4f42
HMDSS <halilmertdeveliii@gmail.com>
2024-05-26 13:53:30 +0300

    model binding for update opration

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,11 +5,11 @@ using System.Diagnostics;
 namespace RepeatOperationForAspNet.Controllers
 {
     public class HomeController : Controller
-    {   
+    {
         public IActionResult Index()
         {
             List<Student> studentList = StudentContext.studentList;
-            
+
             return View(studentList);
         }
         public IActionResult Create()
@@ -17,17 +17,15 @@ namespace RepeatOperationForAspNet.Controllers
             return View(new Student());
         }
         [HttpPost]
-        public IActionResult CreateNewStudent(Student student)
+        public IActionResult Create(Student student)
         {
 
 
-            //string formStudentFirstName = HttpContext.Request.Form["studentName"].ToString();
-            //string formStudentLastName = HttpContext.Request.Form["studentLastName"].ToString();
-            //int formStudentGrade = int.Parse(HttpContext.Request.Form["studentGrade"].ToString());
+
 
 
             Student lastStudent = null;
-            if (StudentContext.studentList.Count>0)
+            if (StudentContext.studentList.Count > 0)
             {
                 lastStudent = StudentContext.studentList.Last();
             }
@@ -41,16 +39,16 @@ namespace RepeatOperationForAspNet.Controllers
             }
 
             StudentContext.studentList.Add(student);
-            
+
 
             return RedirectToAction("Index");
         }
-      
+
         public IActionResult Remove(int id)
         {
 
             var willRemoveStudent = StudentContext.studentList.Find(c => c.StudentId == id);
-            
+
             if (willRemoveStudent != null)
             {
                 StudentContext.studentList.Remove(willRemoveStudent);
@@ -59,21 +57,20 @@ namespace RepeatOperationForAspNet.Controllers
 
         }
         [HttpGet]
-        public  IActionResult Update(int id)
+        public IActionResult Update(int id)
         {
             Student willUpdateStudent = StudentContext.studentList.FirstOrDefault(studentInstance => studentInstance.StudentId == id);
 
             return View(willUpdateStudent);
         }
         [HttpPost]
-        public IActionResult Update()
+        public IActionResult Update(Student student)
         {
-            var id = int.Parse(HttpContext.Request.Form["studentId"].ToString());
-            var willUpdateStudent = StudentContext.studentList.Find(studentInstance => studentInstance.StudentId == id);
+            var willUpdateStudent = StudentContext.studentList.Find(studentInstance => studentInstance.StudentId == student.StudentId);
 
-            willUpdateStudent.StudentName = HttpContext.Request.Form["studentName"].ToString();
-            willUpdateStudent.StudentLastName = HttpContext.Request.Form["studentLastName"].ToString();
-            willUpdateStudent.StudentGrade = int.Parse(HttpContext.Request.Form["studentGrade"].ToString());
+            willUpdateStudent.StudentName = student.StudentName;
+            willUpdateStudent.StudentLastName = student.StudentLastName;
+            willUpdateStudent.StudentGrade = student.StudentGrade;
 
 
             return RedirectToAction("Index");
